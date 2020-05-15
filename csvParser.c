eca5a899db0dd675bbb822f7cf72ec076e7f64a3
Michael Spracklen <michael.spracklen@fullstackacademy.com>
2020-05-15 15:39:12 -0700

    Established TODOs and Added date library

--- csvParser.c
+++ csvParser.c
@@ -4,6 +4,16 @@
 #define RED "\033[0;31m"
 #define DEFAULT "\033[0m"
 
+#define FBUFLEN 4096
+#define LBUFLEN 1024
+
+// TODO: Get to a point where we can verify code is being read correctly by outputting the csv to terminal.
+// TODO: Create a struct that organizes all 5 fields.
+// TODO: Establish a dynamically allocated array of covid_struct
+// TODO: Date Parsing, need to be able to parse dates from text to ensure dates are in order.
+// TODO: Implement a sort algorithm to sort the array by date from earliest to latest.
+// TODO: Generate output from the sorted array. 
+
 // These are called protoypes. They allow us to implement main first without the compiler freaking out.
 // The functions are implemented below the main function.
 
@@ -14,9 +24,9 @@ void push(int* set, int new, int length);
 
 int main(int argc, char *argv[]) {
     
-    static char fileBuffer[4096]; // A 4096 character buffer for fread to feed.
-    static char *fieldBuffer = malloc()
-    int size, lineLength;
+    static char fileBuffer[FBUFLEN]; // A 4K character buffer for fread to feed.
+    static char fieldBuffer[LBUFLEN]; // A generous line buffer.
+    int size; // The size in bytes of the data read back from fread.
     int fieldLength=0;
     char* current;
     if (argc != 3) {
@@ -28,33 +38,45 @@ int main(int argc, char *argv[]) {
     }
     char *ISO = argv[2];
     char *path = argv[1];
+
+    // index 0: ISO country code
+    // index 1: Location
+    // index 2: Date
+    // index 3: total_cases
+    // Index 4: new_cases
     int positions[] = {-1, -1, -1, -1, -1};
 
-    FILE *pFile = fopen (argv[1], "r");
+    FILE *pFile = fopen (argv[1], "r"); // Open the file
+
+    // Error handling for failed open.
     if (pFile == NULL) {
         fprintf(stderr, RED);
         fprintf(stderr, "Could not open %s.", argv[1]);
         fprintf(stderr, DEFAULT);
         exit(1);
     }
-    //Feed the file into buffer, 4096 bytes at a time.
-    while (0 < (size = fread(fileBuffer, 1, sizeof(fileBuffer), pFile))) {
 
+    //Feed the file into buffer, 4096 chars at a time until there is nothing to read.
+    while (0 < (size = fread(fileBuffer, 1, sizeof(fileBuffer), pFile))) {
+        // Reset the position of the current character, every buffer pull.
         int position = 0;
         // Read lines until told to break
-        while (TRUE) {
-            // Read until the end of the line or the end of the file
-            lineLength=0;
-            for(; position < size && buffer[position] != '\n'; position++) {
-                if (buffer[position] != ',') {
-                    fieldBuffer[fieldPos]
+        while (1) {
+
+            // Read until the end of the line or the end of the buffer
+            int lineLength; // The farthest position of the current line
+            for(lineLength = 1; position < size && fileBuffer[position] != '\n'; position++) {
+                // Check for the end of the current field.
+                if (fileBuffer[position] != ',') {
+                    //TODO: Implement checking ISO
                 }
                 lineLength++;
             }
             //At the end of the buffer, rewind the file to the beginning of the current line
-            if(position == size) {
-                if (size==sizeof(fileBuffer)) {
-                    fseek(pFile, (-1 * lineLength), SEEK_CUR)
+            if(position == size) { // TODO: Make sure the position would be the size at the end.
+                if (size==FBUFLEN) {
+                    // lineLength should be one greater than the index of the farthest character, allowing fread to start at the beginning of the next line.
+                    fseek(pFile, (-1 * lineLength), SEEK_CUR);
                 }
                 break;
             } else {
@@ -79,9 +101,12 @@ int STRCMP(const char* str1, const char* str2) {
 
     return c1 - c2;
 }
+
 void push(int* set, int new, int length) {
+    // set every value from index 0 through index 6 to be the value thats ahead of it.
     for(int i = 1; i < length; i++) {
         set[i-1] = set[i];
     }
+    // set the value of the final index to be new
     set[length-1] = new;
 }
---
+++ dateLib.h
@@ -0,0 +1,4 @@
+//Process two dates and return a positive value if date1 is greater, 0 if equal and a negative value if date1 is the lesser
+int compareDates(char* date1, char* date2){
+
+}
