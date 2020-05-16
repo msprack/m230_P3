d6a3fabb2e96bc02aaacff91fe23c8e35d7e9248
Ricardo Lemus <irdetailz@Gmail.com>
2020-05-15 17:14:44 -0700

    Add files via upload

--- dateLib.h
+++ dateLib.h
@@ -1,4 +1,11 @@
 //Process two dates and return a positive value if date1 is greater, 0 if equal and a negative value if date1 is the lesser
-int compareDates(char* date1, char* date2){
+int compareDates(char* date1, char* date2)
+{
+    while (*date1 != '\0')
+    {
+        if(*date2 > *date1) return -1'
+        if (*date1 > *date2) return 1;
+        if( *date1 == *date2) return 0;
 
+    }
 }
